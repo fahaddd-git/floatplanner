a791c73b34920583d186bc3ea15db8b90a7d9ab6
fahad <awan.fahad@gmail.com>
2021-04-27 21:43:49 -0500

    added more data

--- planner.js
+++ planner.js
@@ -25,7 +25,7 @@ let hydroOverlay = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/serv
 
 // style for polyline
 let lineStyle = {
-    "color": "#ff7800",
+    "color": "#3E91AF",
     "weight": 5,
     "opacity": 0.65
 };
@@ -170,13 +170,46 @@ $.when($.getJSON("currentRiverDM.geojson"), $.getJSON("currentRiverSites.geojson
         markersFeatureGroup.addTo(map)
 
         // // zoom the map to the polyline
-        // map.fitBounds(polyline.getBounds());
+        map.fitBounds(navigationOverlay.getBounds());
+        
+        
+        function deg2rad(deg)
+        {
+            return deg * (Math.PI/180);
+        }
+
+        function haversine(pair1, pair2)
+        {
+            let lat1=pair1.lat
+          
+            let lon1=pair1.lng
+          
+            let lat2=pair2.lat
+            let lon2=pair2.lng
+           // console.log(`lat1: ${lat1}  lon1: ${lon1}  lat2: ${lat2}  lon2: ${lon2}`)
+            const R = 3971.366
+            // Radius of the earth in mi with using formula 3963 - 13 * math.sin(37) where 37 is latitude in Missouri
+            
+        
+            var dLat = deg2rad(lat2-lat1);
+            var dLon = deg2rad(lon2-lon1);
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                    Math.sin(dLon/2) * Math.sin(dLon/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        
+            // Return distance in miles.
+            return R * c;
+        }
 
 
+        //variables for the counter and start/end coords
         let counter=0
         let start;
         let end;
-        map.on('click', function(e){
+
+        //BUG: toggling navigation overlay in map legend brings it above the user added polyline causing color issues
+        navigationOverlay.on('click', function(e){
             if(counter==0){
                start=(L.GeometryUtil.locateOnLine(map, navigationOverlay, e.latlng))
                 console.log(`start=${start}`)
@@ -189,10 +222,15 @@ $.when($.getJSON("currentRiverDM.geojson"), $.getJSON("currentRiverSites.geojson
                console.log(`end=${end}`)
                let lineSubset=L.GeometryUtil.extract(map, navigationOverlay, start, end)
                console.log(lineSubset)
-               let testSubset=new L.Polyline(lineSubset).setStyle({color: 'black'}).addTo(map)
+               let testSubset=new L.Polyline(lineSubset).setStyle({color: '#AF4C3E'}).addTo(map)
                
-               console.log(L.GeometryUtil.length(lineSubset));
+               let totalDistance = 0
+            
+               for (let i = 1; i < lineSubset.length; i++) {
+                     totalDistance += haversine(lineSubset[i-1], lineSubset[i])
+                }
                counter=0
+               console.log(totalDistance)
                return
           
                 
